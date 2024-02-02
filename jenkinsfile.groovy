ed10ab4d868f5e6f3b41305bf399b40c268faf98
Neliembp <85373420+ShammaLove@users.noreply.github.com>
2024-02-01 23:35:28 -0800

    adjusting Jenkinsfile

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,46 +1,45 @@
 pipeline {
-    agent any  // This means the pipeline can run on any available agent (node/agent) in the Jenkins environment
+    agent any
 
     stages {
         stage('Checkout') {
             steps {
-                // This step checks out the source code from your version control system (e.g., Git)
-                checkout scm
+                script {
+                    // Update 'YOUR-CREDENTIALS-ID' with the actual ID of your Jenkins credentials
+                    checkout([$class: 'GitSCM', branches: [[name: '*/master']], 
+                               userRemoteConfigs: [[url: 'https://github.com/NelieTchat/CloudNet.CFN.git', 
+                               credentialsId: '12345678']]])
+                }
             }
         }
 
         stage('Build') {
             steps {
-                // This is where you can define the build steps for your project
                 sh 'echo "Building your project"'
-                // You can add more build commands or use tools like Maven, Gradle, etc.
+                // Add build steps as needed
             }
         }
 
         stage('Test') {
             steps {
-                // This is where you can define the test steps for your project
                 sh 'echo "Running tests"'
-                // You can add commands to run your tests here
+                // Add test steps as needed
             }
         }
 
         stage('Deploy') {
             steps {
-                // This is where you can define the deployment steps for your project
                 sh 'echo "Deploying your project"'
-                // You can add commands to deploy your application to a server or a cloud platform
+                // Add deployment steps as needed
             }
         }
     }
 
     post {
         success {
-            // This block will be executed if the pipeline succeeds
             echo 'Pipeline succeeded! You can add further actions here.'
         }
         failure {
-            // This block will be executed if the pipeline fails
             echo 'Pipeline failed! You can add further actions for failure handling.'
         }
     }
